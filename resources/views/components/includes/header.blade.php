ce3ea68295e8c880c3843817edaf6691ba54d3d8
yunusemremn <yunusemre.ozturk@modanisa.com>
2021-12-03 01:32:19 +0300

    fx

--- resources/views/components/includes/header.blade.php
+++ resources/views/components/includes/header.blade.php
@@ -12,63 +12,8 @@
         <div class="responsive-mobile-menu">
             <div class="logo-wrapper mobile-logo">
                 <a href="index.html" class="logo">
-                    <img src="assets\img\logo.png" alt="logo">
+                    <img src="assets\img\exxefy-logo.png" width="150px" alt="logo">
                 </a>
-            </div>
-            <div class="nav-right-content">
-                <ul>
-                    <li class="search">
-                        <i class="ti-search"></i>
-                    </li>
-                    <li class="cart">
-                        <div class="cart-icon"><i class="la la-shopping-cart"></i></div>
-                        <div class="widget_shopping_cart_content">
-                            <ul>
-                                <li>
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <img src="assets\img\checkout\7.png" alt="img">
-                                        </div>
-                                        <div class="media-body">
-                                            <a class="title" href="#">Smart watch red color</a>
-                                            <p>Quantity: 1</p>
-                                            <span class="price">$150.00</span>
-                                        </div>
-                                    </div>
-                                    <a class="remove-product" href="#"><span class="ti-close"></span></a>
-                                </li>
-                                <li>
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <img src="assets\img\checkout\8.png" alt="img">
-                                        </div>
-                                        <div class="media-body">
-                                            <a class="title" href="#">Smart watch red color</a>
-                                            <p>Quantity: 1</p>
-                                            <span class="price">$150.00</span>
-                                        </div>
-                                    </div>
-                                    <a class="remove-product" href="#"><span class="ti-close"></span></a>
-                                </li>
-                            </ul>
-                            <p class="total">
-                                <strong>Subtotal:</strong>
-                                <span class="amount">
-                                    <span class="woocommerce-Price-currencySymbol">$</span>129.00
-                                </span>
-                            </p>
-                            <p class="buttons">
-                                <a href="checkout.html" class="button">View Card & Check out</a>
-                            </p>
-                        </div>
-                    </li>
-                    <li class="notification">
-                        <a href="#">
-                            <i class="fa fa-heart-o"></i>
-                            <span class="notification-count">0</span>
-                        </a>
-                    </li>
-                </ul>
             </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Riyaqas_main_menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggle-icon">
@@ -81,26 +26,15 @@
         <div class="collapse navbar-collapse" id="Riyaqas_main_menu">
             <div class="logo-wrapper desktop-logo">
                 <a href="index.html" class="logo">
-                    <img src="assets\img\logo.png" alt="logo">
+                    <img src="assets\img\exxefy-logo.png" width="150px" alt="logo">
                 </a>
             </div>
             <ul class="navbar-nav">
-                <li class="menu-item-has-children">
-                    <a href="#">Home</a>
-                    <ul class="sub-menu">
-                        <li><a href="index.html">Software Home</a></li>
-                        <li><a href="home02.html">App Home</a></li>
-                        <li><a href="home03.html">Startup Company</a></li>
-                        <li><a href="home04.html">Management software</a></li>
-                        <li><a href="home05.html">Tech-company</a></li>
-                        <li><a href="home06.html">New Technology</a></li>
-                        <li><a href="home07.html">Digital Marketing</a></li>
-                        <li><a href="home08.html">Pos Device</a></li>
-                        <li><a href="home09.html">Hosting</a></li>
-                    </ul>
+                <li>
+                    <a href="#">Anasayfa</a>
                 </li>
                 <li class="menu-item-has-children">
-                    <a href="#">Shop</a>
+                    <a href="#">Hemen Başla</a>
                     <ul class="sub-menu">
                         <li><a href="shop.html">Shop</a></li>
                         <li><a href="shop-grid.html">Shop Grid</a></li>
@@ -109,7 +43,7 @@
                     </ul>
                 </li>
                 <li class="menu-item-has-children">
-                    <a href="#">Blog</a>
+                    <a href="#">Fiyatlandırma</a>
                     <ul class="sub-menu">
                         <li><a href="blog.html">Blog</a></li>
                         <li><a href="blog-grid.html">Blog Grid</a></li>
@@ -117,7 +51,7 @@
                     </ul>
                 </li>
                 <li class="menu-item-has-children">
-                    <a href="#">Pages</a>
+                    <a href="#">Blog</a>
                     <ul class="sub-menu">
                         <li><a href="about.html">About</a></li>
                         <li><a href="service.html">Service</a></li>
@@ -131,16 +65,8 @@
                         <li><a href="work-processing.html">Work Processing</a></li>
                     </ul>
                 </li>
-                <li class="menu-item-has-children">
-                    <a href="#">Career</a>
-                    <ul class="sub-menu">
-                        <li><a href="job-listing.html">Job listing</a></li>
-                        <li><a href="job-details.html">Job Details</a></li>
-                        <li><a href="job-apply.html">Job Apply</a></li>
-                    </ul>
-                </li>
                 <li>
-                    <a href="contact.html">Contact</a>
+                    <a href="contact.html">İletişim</a>
                 </li>
             </ul>
         </div>
