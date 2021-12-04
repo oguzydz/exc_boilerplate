4ca384ea28fab490ac5a05acfb2746205773c2b9
ismetym <ismet@yunusemre.me>
2021-12-04 20:25:48 +0300

    cfx

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesAndPermissionsSeeder::class,
+            CitySeeder::class,
+            CountiesSeeder::class
         ]);
     }
 }
