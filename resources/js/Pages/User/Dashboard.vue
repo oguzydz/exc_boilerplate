ebabebfaae93bd7ab16bf4ef2f9fbe9706885dbc
yunusemremn <yunusemre.ozturk@modanisa.com>
2021-12-08 08:37:43 +0300

    dash

--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -2,12 +2,17 @@
   <app-layout title="Stok Görselleri">
     <div class="row">
       <div class="col-sm-12 col-lg-12 col-md-12">
-
-            <el-steps :active="1" finish-status="success" simple style="margin-top: 20px">
-            <el-step title="Step 1" ></el-step>
-            <el-step title="Step 2" ></el-step>
-            <el-step title="Step 3" ></el-step>
-            </el-steps>
+        <el-steps
+          :active="active"
+          finish-status="success"
+          simple
+          style="margin-top: 20px"
+        >
+          <el-step title="Step 1"></el-step>
+          <el-step title="Step 2"></el-step>
+          <el-step title="Step 3"></el-step>
+        </el-steps>
+        <el-button style="margin-top: 12px;" @click="next">Next step</el-button>
 
       </div>
     </div>
@@ -15,11 +20,23 @@
 </template>
 
 <script>
-import AppLayout from '@/Layouts/AppUserLayout'
+import AppLayout from "@/Layouts/AppUserLayout";
 
 export default {
   components: {
     AppLayout,
   },
-}
+
+  data() {
+    return {
+      active: 0,
+    };
+  },
+
+  methods: {
+    next() {
+      if (this.active++ > 2) this.active = 0;
+    },
+  },
+};
 </script>
