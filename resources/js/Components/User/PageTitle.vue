dc603c5f0c5adaa3bdb422253f30ab2bb662e671
yunusemremn <yunusemre.ozturk@modanisa.com>
2021-12-18 17:18:46 +0300

    user dashboard

---
+++ resources/js/Components/User/PageTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <h6 class="page-title">{{ title }}</h6>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+}
+</script>
+
+<style scoped>
+.page-title {
+    font-size: 12px;
+    font-weight: bold;
+}
+</style>
--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <app-layout title="Stok Görselleri">
+    <page-title :title="title" />
     <div class="row">
       <div class="col-sm-12 col-lg-12 col-md-12">
         <el-steps
@@ -8,9 +9,10 @@
           simple
           style="margin-top: 20px"
         >
-          <el-step title="Step 1"></el-step>
-          <el-step title="Step 2"></el-step>
-          <el-step title="Step 3"></el-step>
+          <el-step title="Üyelik Bilgileri"></el-step>
+          <el-step title="Banka Hesabı"></el-step>
+          <el-step title="Satılan Ürün"></el-step>
+          <el-step title="Tamamla"></el-step>
         </el-steps>
         <el-button style="margin-top: 12px;" @click="next">Next step</el-button>
 
@@ -19,17 +21,26 @@
   </app-layout>
 </template>
 
+<style>
+.el-step.is-simple .el-step__title {
+    font-size: 12px;
+}
+</style>
+
 <script>
 import AppLayout from "@/Layouts/AppUserLayout";
+import PageTitle from "@/Components/User/PageTitle";
 
 export default {
   components: {
     AppLayout,
+    PageTitle,
   },
 
   data() {
     return {
       active: 0,
+      title: 'ÜCRETSİZ HESAP OLUŞTURUN',
     };
   },
 
