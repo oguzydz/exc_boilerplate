47830e4d11a31e7ad3c583700e867ba2d98cc0ff
yunusemremn <yunusemre.ozturk@modanisa.com>
2021-12-26 18:55:12 +0300

    shop mobile

--- resources/views/components/shop/header.blade.php
+++ resources/views/components/shop/header.blade.php
@@ -90,25 +90,24 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="cover-body d-flex justify-content-between align-items-center">
-                                <div>
-                                    <img class="profile-pic" src="https://bootdey.com/img/Content/avatar/avatar6.png"
-                                        alt="profile">
-                                    <span class="profile-name">Amiah Burton</span>
-                                    <p class="d-inline-block">
-                                        asdasda
-                                    </p>
+                            <div class="row cover-body justify-content-between align-items-center">
+                                <div class="col-md-8">
+                                    <div class="row align-items-center">
+                                        <div class="col-md-2 col-3">
+                                            <img class="profile-pic"
+                                                src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="profile">
+                                        </div>
+                                        <div class="col-md-10 col-9">
+                                            <span class="profile-name">Amiah Burton</span>
+                                            <p class="">
+                                                Yumuşacık ve sağlıklı pamukları doğanın renkleriyle buluşturduk. Şimdi sıra size ulaştırmakta.
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="d-none d-md-block">
-                                    <button class="btn btn-primary btn-icon-text btn-edit-profile">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-edit btn-icon-prepend">
-                                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                        </svg> Edit profile
-                                    </button>
+                                <div class="d-none d-md-block col-md-4 text-right">
+                                    <a class="btn btn-green text-white" href="http://127.0.0.1:5421/register">Abone
+                                        Ol!</a>
                                 </div>
                             </div>
                         </div>
