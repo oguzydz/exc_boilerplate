be3efe7cbddfcf3fefbdd81d0c527332e2bf36fa
beatrax <get@yunusemre.me>
2022-02-06 18:58:53 +0300

    fx

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \RealRashid\SweetAlert\ToSweetAlert::class,
+            \App\Http\Middleware\RedirectToNonWwwMiddleware::class
         ],
 
         'api' => [
