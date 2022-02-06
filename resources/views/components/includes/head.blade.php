56807b3453dfacd49952e1b26c954d23261deb3c
beatrax <get@yunusemre.me>
2022-02-07 01:18:42 +0300

    fx

--- resources/views/components/includes/head.blade.php
+++ resources/views/components/includes/head.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta name="google-site-verification" content="y31mwpXJsGC8RMDAKo5d4C0xRv_a4Bx6vewQL98nyo8" />
     <link rel="icon" href="{{ asset('assets\img\exxefy-fav.png') }}" sizes="20x20" type="image/png">
     <link rel="stylesheet" href="{{ asset('assets\css\animate.css') }}">
     <link rel="stylesheet" href="{{ asset('assets\css\bootstrap.min.css') }}">
