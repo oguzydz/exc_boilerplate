aae7f3fed902dfc11b1206ec8b09e637f2e88a1c
yunusemremn <yunusemre.ozturk@modanisa.com>
2022-02-08 22:13:32 +0300

    comp assets

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -9,7 +9,7 @@ class Company extends Model
 {
     use HasFactory;
 
-    const EXAMPLE_LOGO_URL = 'company-assets/example-logo.png';
+    const EXAMPLE_LOGO_URL = 'company-assets/example-logov.jpg';
     const EXAMPLE_BG_URL = 'company-assets/example-bg.png';
 
     public $fillable = [
