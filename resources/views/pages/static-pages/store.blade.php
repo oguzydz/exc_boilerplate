d3013f8608c50cd24753679b8fa3562ac2552eca
akiferbey <akiferbey99@gmail.com>
2022-08-16 10:25:46 +0300

    link fx

--- resources/views/pages/static-pages/store.blade.php
+++ resources/views/pages/static-pages/store.blade.php
@@ -28,7 +28,7 @@ kolaylıkla kavuşturmakta')
                         <div class="thumb">
                             <img src="assets\img\stores\evabid-magaza.jpg" alt="team">
                         </div>
-                        <h6><a href="exxefy.com/evabidhome">Evabid Home</a></h6>
+                        <h6><a href="https://exxefy.com/evabidhome">Evabid Home</a></h6>
                         <span>Dekoratif Ev Ürünleri</span>
                     </div>
                 </div>
@@ -37,7 +37,7 @@ kolaylıkla kavuşturmakta')
                         <div class="thumb">
                             <img src="assets\img\stores\enmoda.jpg" alt="team">
                         </div>
-                        <h6><a href="exxefy.com/enmoda">Enmoda</a></h6>
+                        <h6><a href="https://exxefy.com/enmoda">Enmoda</a></h6>
                         <span>Kadın-Erkek Giyim</span>
                     </div>
                 </div>
@@ -46,7 +46,7 @@ kolaylıkla kavuşturmakta')
                         <div class="thumb">
                             <img src="assets\img\stores\exxestock.jpg" alt="team">
                         </div>
-                        <h6><a href="exxefy.com/exxestock">Exxestock</a></h6>
+                        <h6><a href="https://exxefy.com/exxestock">Exxestock</a></h6>
                         <span>Dijital Lisans Ürünleri</span>
                     </div>
                 </div>
@@ -55,7 +55,7 @@ kolaylıkla kavuşturmakta')
                         <div class="thumb">
                             <img src="assets\img\stores\naidesing-logo.jpeg" alt="team">
                         </div>
-                        <h6><a href="exxefy.com/naidesing">NaiDesing</a></h6>
+                        <h6><a href="https://exxefy.com/naidesing">NaiDesing</a></h6>
                         <span>Giyim Markası</span>
                     </div>
                 </div>
