adf2a1fc772267d331926382686bc48cd0f01755
beatrax <get@yunusemre.me>
2022-08-25 16:21:23 +0300

    fx

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -4,7 +4,19 @@
 <x-includes.head />
 
 <body class="light-version">
-
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/630776ff37898912e965216d/1gbaihm3f';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
     <x-includes.header />
 
     @yield('content')
