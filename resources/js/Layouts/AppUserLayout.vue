e286f0be19d7827157facd0a35dd11c34e36e7a4
beatrax <get@yunusemre.me>
2022-08-25 16:38:49 +0300

    fx

--- resources/js/Layouts/AppUserLayout.vue
+++ resources/js/Layouts/AppUserLayout.vue
@@ -1,30 +1,19 @@
 <template>
-  <web-title :title="title" />
-  <div
-    :class="
-      drawerMenuVisible === true && !isMobile()
-        ? 'wrapper d-flex flex-column min-vh-100 bg-light'
-        : 'd-flex flex-column min-vh-100 bg-light'
-    "
-  >
-    <Sidebar
-      :drawerMenuVisible="drawerMenuVisible"
-      :toggleDrawerMenu="toggleDrawerMenu"
-      :drawerMenuFoldable="drawerMenuFoldable"
-    />
-    <Header
-      :drawerMenuVisible="drawerMenuVisible"
-      :toggleDrawerMenu="toggleDrawerMenu"
-      :drawerMenuFoldable="drawerMenuFoldable"
-    />
-    <main
-      class="container-fluid sm:px-0"
-      style="min-height: 100vh; padding-bottom: 50px"
-    >
-      <slot></slot>
-    </main>
-    <Footer />
-  </div>
+    <web-title :title="title" />
+    <div :class="
+        drawerMenuVisible === true && !isMobile()
+            ? 'wrapper d-flex flex-column min-vh-100 bg-light'
+            : 'd-flex flex-column min-vh-100 bg-light'
+    ">
+        <Sidebar :drawerMenuVisible="drawerMenuVisible" :toggleDrawerMenu="toggleDrawerMenu"
+            :drawerMenuFoldable="drawerMenuFoldable" />
+        <Header :drawerMenuVisible="drawerMenuVisible" :toggleDrawerMenu="toggleDrawerMenu"
+            :drawerMenuFoldable="drawerMenuFoldable" />
+        <main class="container-fluid sm:px-0" style="min-height: 100vh; padding-bottom: 50px">
+            <slot></slot>
+        </main>
+        <Footer />
+    </div>
 </template>
 
 <script>
@@ -34,73 +23,75 @@ import Header from "@/Components/User/Header";
 import WebTitle from "@/Components/User/Title";
 
 export default {
-  components: {
-    WebTitle,
-    Footer,
-    Sidebar,
-    Header,
-  },
-
-  props: {
-    title: String,
-  },
-
-  data() {
-    return {
-      drawerMenuVisible: true,
-      drawerMenuFoldable: false,
-    };
-  },
-
-  methods: {
-    logout() {
-      this.$inertia.post(route("logout"));
+    components: {
+        WebTitle,
+        Footer,
+        Sidebar,
+        Header,
     },
 
-    toggleDrawerMenu() {
-      this.drawerMenuVisible = !this.drawerMenuVisible;
-      console.log(this.drawerMenuVisible);
+    props: {
+        title: String,
     },
-    toggleDrawerMenuFoldable() {
-      this.drawerMenuFoldable = !this.drawerMenuFoldable;
-      console.log(this.drawerMenuFoldable);
+
+    data() {
+        return {
+            drawerMenuVisible: true,
+            drawerMenuFoldable: false,
+        };
     },
-    isMobile() {
-      if (
-        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-          navigator.userAgent
-        )
-      ) {
-        return true;
-      } else {
-        return false;
-      }
+
+    methods: {
+        logout() {
+            this.$inertia.post(route("logout"));
+        },
+
+        toggleDrawerMenu() {
+            this.drawerMenuVisible = !this.drawerMenuVisible;
+            console.log(this.drawerMenuVisible);
+        },
+        toggleDrawerMenuFoldable() {
+            this.drawerMenuFoldable = !this.drawerMenuFoldable;
+            console.log(this.drawerMenuFoldable);
+        },
+        isMobile() {
+            if (
+                /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                    navigator.userAgent
+                )
+            ) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        sidebarActive() {
+            if (this.isMobile()) {
+                this.drawerMenuVisible = false;
+            }
+        },
     },
-    sidebarActive() {
-      if (this.isMobile()) {
-        this.drawerMenuVisible = false;
-      }
+    mounted: function () {
+        this.$nextTick(this.sidebarActive());
     },
-  },
-  mounted: function () {
-    this.$nextTick(this.sidebarActive());
-  },
 };
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/618127206bb0760a4940c41d/1fjg81mf0';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/630776ff37898912e965216d/1gbaihm3f';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
 })();
 
 </script>
 <style>
 .wrapper {
-  padding-left: 256px;
-  transition: padding-left 0.1s ease 0.1s;
+    padding-left: 256px;
+    transition: padding-left 0.1s ease 0.1s;
 }
 </style>
