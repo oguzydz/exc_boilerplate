238d1379cd270c6e27a166ed73138a16b77ed16a
beatrax <get@yunusemre.me>
2022-08-25 23:37:37 +0300

    fx

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -21,6 +21,9 @@
                                     <div class="btn-wrapper desktop-left padding-top-20">
                                         <a href="{{ asset('register') }}" class="btn btn-radius btn-green s-animate-3">Hemen
                                             Kayıt Ol</a>
+                                        <a href="https://www.youtube.com/watch?v=anYufqsUCAU"
+                                            class="btn btn-radius btn-green s-animate-3 ml-md-2 mt-2 mt-md-0 video-play-btn mfp-iframe">Tanıtım
+                                            Videosu</a>
                                     </div>
                                 </div>
                             </div>
@@ -242,8 +245,9 @@
                         <div class="img-wrap">
                             <img src="assets\img\we-provide\01.png" alt="video">
                             <div class="hover">
-                                <a href="#" class="video-play-btn mfp-iframe"><img
-                                        src="assets\img\we-provide\3.svg" alt="video"></a>
+                                <a href="https://www.youtube.com/watch?v=anYufqsUCAU"
+                                    class="video-play-btn mfp-iframe"><img src="assets\img\we-provide\3.svg"
+                                        alt="video"></a>
                             </div>
                         </div>
                     </div>
