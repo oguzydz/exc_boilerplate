f214e2cb22ac6b551db41b39df4796a52ca4de12
yunusemremn <yunusemre.ozturk@modanisa.com>
2022-09-02 20:28:11 +0300

    fx

--- resources/js/Pages/User/Category/Create.vue
+++ resources/js/Pages/User/Category/Create.vue
@@ -1,144 +1,26 @@
 <template>
-    <FilePond />
+    <div id="app">
+        <FilePondDemo />
+    </div>
 </template>
 
-<script>
-import AppLayout from "@/Layouts/AppUserLayout";
-import PageTitle from "@/Components/User/PageTitle";
-import FilePondx from "@/Components/Shared/FilePond";
-
-import vueFilePond from "vue-filepond";
-import "filepond/dist/filepond.min.css";
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
-import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
-
-const FilePond = vueFilePond(
-    FilePondPluginFileValidateType,
-    FilePondPluginImagePreview
-)
+  <script>
+import FilePondDemo from "@/Components/Shared/FilePond";
 
 export default {
+    name: 'app',
     components: {
-        AppLayout,
-        FilePond,
-        FilePondx,
-        PageTitle,
-    },
-    props: {
-        errors: {},
-    },
-
-    data() {
-        return {
-            title: "ÜCRETSİZ HESAP OLUŞTURUN",
-            rules: {
-                title: [
-                    {
-                        required: true,
-                        message: "Lütfen ürün adı alanını doldurunuz.",
-                        trigger: "blur",
-                    },
-                    {
-                        min: 3,
-                        max: 40,
-                        message: "En küçük 3, en fazla 40 karakter olabilir.",
-                        trigger: "blur",
-                    },
-                ],
-                text: [
-                    {
-                        required: true,
-                        message: "Lütfen ürün açıklaması giriniz.",
-                        trigger: "blur",
-                    },
-                ],
-                // image: [
-                //     {
-                //         required: true,
-                //         message: "Lütfen görsel giriniz.",
-                //         trigger: "blur",
-                //     },
-                // ],
-                order: [
-                    {
-                        required: true,
-                        message: "Lütfen ürün sırası giriniz.",
-                        trigger: "blur",
-                    },
-                    {
-                        min: 1,
-                        max: 1000,
-                        trigger: "blur",
-                    },
-                ],
-            },
-            form: {
-                title: null,
-                text: null,
-                image: null,
-                order: null,
-            },
-        };
-    },
-
-    watch: {
-        status: function (val) {
-            this.setStatus(val);
-        },
-    },
-
-    methods: {
-        setStatus(status) {
-            this.status = status;
-        },
-
-        onAddFile(error, file) {
-            if (error) {
-                this.$message({
-                    type: "error",
-                    dangerouslyUseHTMLString: true,
-                    message:
-                        "Görsel Eklenirken bir hata ile karşılaşıldı. Tekrar Deneyiniz!",
-                });
-            }
-            this.form.image = file.source;
-        },
-        submitForm(formName) {
-            this.$refs[formName].validate((valid) => {
-                if (valid) {
-                    if (formName == "form") {
-                        this.$inertia.post(
-                            route("user.category.store"),
-                            this.form,
-                            {
-                                onSuccess: (page) => {
-                                   this.$message({
-                                        type: "success",
-                                        message: "İşlem başarıyla tamamlandı.",
-                                    });
-                                    this.$inertia.get(route('user.category.index'))
-                                },
-                                onError: (errors) => {
-                                    this.$message({
-                                        type: "error",
-                                        dangerouslyUseHTMLString: true,
-                                        message:
-                                            "Hata: Aşağıda yazan sorunları düzeltmelisiniz. <br><br>" +
-                                            this.errorsToMessage(errors),
-                                    });
-                                },
-                            }
-                        );
-                    } else {
-                        //if (this.active++ > 2) this.active = 0;
-                    }
-                } else {
-                    console.log("error submit!!");
-                    return false;
-                }
-            });
-        },
-    },
-};
+        FilePondDemo
+    }
+}
 </script>
+
+  <style>
+  #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+  }
+  </style>
