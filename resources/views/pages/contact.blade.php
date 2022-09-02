9b43f7d80bd1ae6910b32d46177d130925370b99
yunusemremn <yunusemre.ozturk@modanisa.com>
2022-09-02 20:33:36 +0300

    fx

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -41,6 +41,7 @@
                             </div>
                             <div class="col-md-12">
                                 <div class="single-input-wrap">
+                                    <input type="file">
                                     <input type="text" name="text" id="subject" class="single-input" required>
                                     <label>Konu</label>
                                 </div>
